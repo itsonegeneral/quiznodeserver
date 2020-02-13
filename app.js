0cafbf4facd848d641fcd539d4b6f0fc6f6d5407
rithiks <srithik1999@gmail.com>
2020-02-13 12:03:43 +0530

    fix

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var app = express();
 
 app.use(cors());
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    
+};
+app.use(allowCrossDomain());
+
+
 
 var mysqlConnection = mysql.createConnection({
     host:'knackdb.cigwhmgnsi5e.us-east-2.rds.amazonaws.com',
