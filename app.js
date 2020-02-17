798252273bb54cda90f4c15db4f7c095ef8f737b
rithiks <srithik1999@gmail.com>
2020-02-17 15:56:50 +0530

    statuses

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  
-
 
 var mysqlConnection = mysql.createConnection({
     host:'knackdb.cigwhmgnsi5e.us-east-2.rds.amazonaws.com',
@@ -31,7 +29,7 @@ mysqlConnection.connect((err)=>{
 });
 
 
-app.listen(process.env.PORT || 5000,()=>console.log('Listening to port'));
+app.listen(8080,()=>console.log('Listening to port'));
 
 app.get('/test',(req,res)=>{
     var query = "SELECT * FROM questions;"
@@ -88,7 +86,38 @@ app.get('/addquestion',(req,res)=>{
     })
 });
 
+app.get('/totalstatus',(req,res)=>{
+    var query = "SELECT count(*),category from questions group by category";
+
+    mysqlConnection.query(query,(err,rows,fields)=>{
+        if(!err){
+            var array = [];
+
+            var response = {
+                status : 'success',
+                data : JSON.parse(JSON.stringify(rows))
+            }
+            res.set(200).json(response);
+        }else{
+            res.set(500).json(err);
+        }
+    });
+});
+
+
+app.get('/userstatus',(req,res)=>{
+    var query = "SELECT count(*),adminemail from questions group by adminemail";
+    mysqlConnection.query(query,(err,rows,fields)=>{
+        if(!err){
+            res.set(200).json(rows);
+        }else{
+            res.set(500).json(err);
+        }
+    });
+});
+
 app.get('/getquestions',(req,res)=>{
+    console.log('Get Questions');
     var category = req.query.category;
     var limit = req.query.limit;
     var query = "SELECT * from questions where category = '" + category +"' ORDER BY RAND() LIMIT " + limit + ";";
