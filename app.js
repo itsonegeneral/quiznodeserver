d94d32658e0675331da9afc7207a326948242748
rithiks <srithik1999@gmail.com>
2020-02-17 16:07:35 +0530

    statuses

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ mysqlConnection.connect((err)=>{
 });
 
 
-app.listen(process.env.PORT|5000,()=>console.log('Listening to port'));
+app.listen(process.env.PORT||5000,()=>console.log('Listening to port'));
 
 app.get('/test',(req,res)=>{
     var query = "SELECT * FROM questions;"
