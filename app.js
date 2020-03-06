d74fa7b17ac2de2f6988d46f268c5902ed39e70a
rithiks <srithik1999@gmail.com>
2020-03-06 11:49:25 +0530

    db new

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mysql = require('mysql');
 const express = require('express');
 const cors = require('cors');
 var app = express();
+var dataArray = [];
+const fs = require('fs');
+
 
 app.use(cors());
 
@@ -15,17 +18,29 @@ app.use(function(req, res, next) {
     next();
   });
 
+  /*
 var mysqlConnection = mysql.createConnection({
     host:'knackdb.cigwhmgnsi5e.us-east-2.rds.amazonaws.com',
-    user : 'knackrootadmin',
+    user : 'knackrootadmin', 
     password:'adminknack',
     port:'3306',
     database : 'knackapp'
 });
 
+*/
+var mysqlConnection = mysql.createConnection({
+    host:'sql12.freemysqlhosting.net',
+    user : 'sql12326048',
+    password:'tulp2jG9H3',
+    port:'3306',
+    database : 'sql12326048'
+});
+
+
 mysqlConnection.connect((err)=>{
     if(!err){
-        console.log('Connected');
+    console.log('Connected');
+
     }else{
         console.log("Failed "+ JSON.stringify(err))
     }
@@ -34,9 +49,6 @@ mysqlConnection.connect((err)=>{
 
 app.listen(process.env.PORT||5000,()=>console.log('Listening to port %d'));
 
-app.get('/test',(req,res)=>{
-    mQuestions.allquestions(req,res,mysqlConnection);
-});
 
 app.get('/time',(req,res)=>{
     var response= {
@@ -73,3 +85,50 @@ app.get('/adminstatus',(req,res)=>{
 app.get('/getcategories',(req,res)=>{
     mAdmin.getCategories(req,res,mysqlConnection);
 });
+
+
+
+//Backup and restore
+/*function saveQuestion(question){
+
+    var quest = question;
+    var query = "INSERT INTO questions " +  '(question,option1,option2,option3,option4,answer,parentCategory,category,level,adminEmail) ' + 
+                    " VALUES ( '" + quest.question + "','" + quest.option1 + "','" + quest.option2 + "','" + quest.option3 + "','" + quest.option4 + "','" + 
+                     quest.answer + "','" + quest.parentCategory + "','" +quest.category + "'," +  quest.level + ",'" + quest.adminEmail+ "');";
+
+    console.log("\n" + query +"\n");
+
+    mysqlConnection.query(query,(err,rows,fields)=>{
+        if(!err){
+            console.log('success added ' + quest.id);
+        }else{
+           console.log('faield at ' + quest.id);
+        }
+    })
+}
+
+
+
+
+        let rawdata = fs.readFileSync('table.json');
+        dataArray = JSON.parse(rawdata);
+    
+        
+        for(let i=0;i<dataArray.length ; i++){
+            console.log("Element " + i + " is " + dataArray[i]);
+            saveQuestion(dataArray[i]);
+        }
+
+
+
+app.get('/restore',(req,res)=>{
+
+    let rawdata = fs.readFileSync('table.json');
+    dataArray = JSON.stringify(rawdata);
+    
+    for(let i=0;i<dataArray.length ; i++){
+        saveQuestion(dataArray[i]);
+    }
+
+});
+*/
