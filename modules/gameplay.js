3c86c98daa926b06f2586910cd34e97e9c43f5d4
rithiks <srithik1999@gmail.com>
2020-07-03 00:30:23 +0530

    m

--- modules/gameplay.js
+++ modules/gameplay.js
@@ -22,15 +22,23 @@ module.exports.setquizoptions = function(req,res,con){
 
 module.exports.addLeaderboardEntry = function(req,res,con){
     console.log(req.body.playerid);
-    let query = "UPDATE leaderboard"
+    let score = parseInt(req.body.score);
+    let query = "UPDATE leaderboard SET totalScore=totalScore+"+score+" WHERE playerID='"+req.body.playerid+"';"
     res.set(200).json({
         s:""
     })
+    con.query(query,(err,rows,feilds)=>{
+        if(!err){
+
+        }else{
+            console.log(err);
+        }
+    });
 }
 
 module.exports.createLeaderboard = function(req,res,con){
     console.log(req.body.playerid);
-    let query = "INSERT INTO leaderboard (playerID) VALUES ('"+req.body.playerid+"');"
+    let query = "INSERT INTO leaderboard (playerID,playerName,profileURL) VALUES ('"+req.body.playerid+"','"+req.body.name+"','"+req.body.profileurl+"');"
     if(req.body.playerid === undefined){
         res.json({
             message:"Player ID required"
@@ -40,7 +48,7 @@ module.exports.createLeaderboard = function(req,res,con){
         if(!err){
             
         }else{
-            console.log(err);
+            res.set(err);
         }
     });
 }
